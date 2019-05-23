4bdbd600658d4facc11b2a9673d19b9a245ca789
tmaeno <tmaeno@bnl.gov>
2019-05-23 15:13:51 +0200

    added reportVersion

--- pandawnutil/wnmisc/misc_utils.py
+++ pandawnutil/wnmisc/misc_utils.py
@@ -27,6 +27,9 @@ def add_user_job_metadata():
         os.rename(jobReport, jobReport+'.org')
     # add
     merged_dict['user_job_metadata'] = tmp_dict
+    # version number
+    if 'reportVersion' not in merged_dict:
+        merged_dict['reportVersion'] = '1.0.0'
     # dump
     with open(jobReport, 'w') as f:
         json.dump(merged_dict, f)
