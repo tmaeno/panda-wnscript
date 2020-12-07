2c8cc59754e31b7de2d1f84a4246586f812e595e
tmaeno <tmaeno@bnl.gov>
2020-12-07 11:42:25 +0100

    ROOT discovery when cmtConfig is unspecified

--- pandawnutil/root/root_utils.py
+++ pandawnutil/root/root_utils.py
@@ -1,3 +1,7 @@
+import re
+import subprocess
+
+
 # get ROOT version and setup strings
 def get_version_setup_string(root_ver, cmt_config):
     if not root_ver:
@@ -7,10 +11,22 @@ def get_version_setup_string(root_ver, cmt_config):
         rootVer += ".00"
     # CVMFS version format
     if not cmt_config:
-        print ("Use i686-slc5-gcc43-opt for ROOT by default when --cmtConfig is unset")
-        rootCVMFS = rootVer + '-' + 'i686-slc5-gcc43-opt'
-    else:
-        rootCVMFS = rootVer + '-' + cmt_config
+        cmt_config = None
+
+        com = "export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase; "\
+              "source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh --quiet; "\
+              "showVersions root"
+        out = subprocess.check_output(com, shell=True, universal_newlines=True)
+        for l in out.split('\n'):
+            if rootVer + '-' in l:
+                cmt_config = re.sub(rootVer + '-', '', l.split()[1])
+        if cmt_config:
+            print ("Use {} for ROOT {} found in ALRB although --cmtConfig was not specified".format(cmt_config,
+                                                                                                    rootVer))
+        else:
+            print ("Use x86_64-centos7-gcc8-opt for ROOT by default when --cmtConfig is unset")
+            cmt_config = 'x86_64-centos7-gcc8-opt'
+    rootCVMFS = rootVer + '-' + cmt_config
     # setup string
     tmpSetupEnvStr = ("export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase; "
                       "source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh --quiet; "
